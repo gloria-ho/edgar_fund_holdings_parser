997df97320328e14980f7ae757941004ba28bca3
Gloria H <silverdragonia@gmail.com>
2019-07-17 23:51:33 -0400

    updating import

--- classes/Edgar.py
+++ classes/Edgar.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as Soup
 
 class Edgar:
 
@@ -11,7 +11,7 @@ class Edgar:
 
   def fetch_query(self, query):
     resp = requests.get(query)
-    soup = BeautifulSoup(resp.text, "html.parser")
+    soup = Soup(resp.text, "html.parser")
     return soup
 
   def ticker_or_cik():
@@ -42,7 +42,7 @@ class Edgar:
     # narrow search params to 13F, parse response
     query_13f = self.query + "&type=13F-HR&dateb=&owner=include&count=40"
     query_13f_resp = requests.get(query_13f)
-    query_13f_soup = BeautifulSoup(query_13f_resp.text, "html.parser")
+    query_13f_soup = Soup(query_13f_resp.text, "html.parser")
     return query_13f_soup
 
   def fetch_13f_filing(self, query_13f):
@@ -54,7 +54,7 @@ class Edgar:
       return None
     doc_url= self.url + button["href"]
     doc_filing_pg = requests.get(doc_url)
-    doc_filing_soup = BeautifulSoup(doc_filing_pg.text, "html.parser")
+    doc_filing_soup = Soup(doc_filing_pg.text, "html.parser")
     return doc_filing_soup
 
   def fetch_13f_report(self, fetch_13f_filing):
@@ -63,7 +63,7 @@ class Edgar:
     doc_query = primary_doc["href"]
     doc_url = self.url + doc_query
     report_resp = requests.get(doc_url)
-    report = BeautifulSoup(report_resp.text, "html.parser")
+    report = Soup(report_resp.text, "html.parser")
     return report
 
   def report(self):
